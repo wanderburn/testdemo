a9ab26d6ec072453745d637a7657b562dcb2e293
wanderburn <a6439840@qq.com>
2017-08-16 20:46:16 +0800

    抓取商品图片的路径
    I am a novice to learn to collect data, and have been working hard

---
+++ test_3.py
@@ -0,0 +1,8 @@
+from urllib import urlopen
+from bs4 import BeautifulSoup
+import re
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsobj = BeautifulSoup(html, "html.parser")
+images = bsobj.findAll("img", {"src": re.compile("\.\.\/img\/gifts\/img.*\.jpg")})
+for image in images:
+    print image["src"]
